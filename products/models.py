25ee40dd2be7227bcb7cc3a1ef2fc2389f1efaf6
daniel <bro.danielx@gmail.com>
2018-02-12 07:11:16 -0500

    lookups

--- products/models.py
+++ products/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     title       = models.CharField(max_length=120)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
-    image       = models.FileField(upload_to=upload_image_path, null=True, blank=True)
+    image       = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
     def __str__(self):
         return self.title
