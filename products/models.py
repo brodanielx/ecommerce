ec83e341a285c4061c013fb200628214417afa68
daniel <bro.danielx@gmail.com>
2018-02-15 07:11:10 -0500

    reverse

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ import os
 from django.db import models
 from django.db.models.signals import pre_save
 from .utils import unique_slug_generator
+from django.urls import reverse
 
 
 def get_filename_ext(filepath):
@@ -57,7 +58,7 @@ class Product(models.Model):
     objects = ProductManager()
 
     def get_absolute_url(self):
-        return "/products/{slug}/".format(slug=self.slug)
+        return reverse('products:detail', kwargs={'slug': self.slug})
 
     def __str__(self):
         return self.title
