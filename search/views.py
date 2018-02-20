ef52b555b6061e31b0c1b07dd2963e34eb3767bc
daniel <bro.danielx@gmail.com>
2018-02-20 15:36:52 -0500

    search  and tags

--- search/views.py
+++ search/views.py
@@ -1,6 +1,6 @@
+
 from django.shortcuts import render
 from django.views.generic import ListView
-
 from products.models import Product
 
 class SearchProductView(ListView):
@@ -17,5 +17,5 @@ class SearchProductView(ListView):
         method_dict = request.GET
         query = method_dict.get('q', None)
         if query is not None:
-            return Product.objects.filter(title__icontains=query)
+            return Product.objects.search(query)
         return Product.objects.featured()
---
+++ tags/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.db.models.signals import pre_save
+from .utils import unique_slug_generator
+from django.urls import reverse
+
+class Tag(models.Model):
+    title       = models.CharField(max_length=120)
+    slug        = models.SlugField()
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    active      = models.BooleanField(default=True)
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
