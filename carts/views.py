e445a3e476dbf72e13681a0dc3953c9efc04cd9a
daniel <bro.danielx@gmail.com>
2018-03-01 15:26:10 -0500

    cart model and cart_id

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
+from .models import Cart
+
 def cart_home(request):
+    # del request.session['cart_id']
+    cart_id = request.session.get('cart_id', None)
+    if cart_id is None:  #and isinstance(cart_id, int):
+        cart_obj = Cart.objects.create(user=None)
+        request.session['cart_id'] = cart_obj.id
+        print('new cart created')
+    else:
+        print('cart id exists')
+        print(cart_id)
+        cart_obj = Cart.objects.get(id=cart_id)
+    # print(request.session)
+    # key = request.session.session_key
+    # print(key)
+    request.session['first_name'] = 'Louis'
     return render(request, "carts/home.html", {})
