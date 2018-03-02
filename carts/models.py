9483c7cb9e6a9c7acb3903e3f5c9f271238467c9
daniel <bro.danielx@gmail.com>
2018-03-02 16:29:07 -0500

    cart - keep session on login

--- carts/models.py
+++ carts/models.py
@@ -1,11 +1,27 @@
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import pre_save
 
 from products.models import Product
 
 User = settings.AUTH_USER_MODEL
 
 class CartManager(models.Manager):
+    def new_or_get(self, request):
+        cart_id = request.session.get('cart_id', None)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count() == 1:
+            new_obj = False
+            cart_obj = qs.first()
+            if request.user.is_authenticated() and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = Cart.objects.new(user=request.user)
+            new_obj = True
+            request.session['cart_id'] = cart_obj.id
+        return cart_obj, new_obj
+
     def new(self, user=None):
         user_obj = None
         if user is not None:
@@ -24,3 +40,8 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+# def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+#
+#
+# pre_save.connect(pre_save_cart_receiver, sender=Cart)
