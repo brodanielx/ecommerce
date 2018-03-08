3b767c0594900d09a8daf4c19617aeb92dd2529f
daniel <bro.danielx@gmail.com>
2018-03-08 07:14:19 -0500

    checkout view, upgrade auth to prep for checkout

---
+++ accounts/views.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import authenticate, login, get_user_model
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import LoginForm, RegisterForm
+
+# Create your views here.
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print(request.user.is_authenticated())
+            print(user)
+            # context['form'] = LoginForm()
+            return redirect("/")
+        else:
+            print('error')
+    return render(request, 'auth/login.html', context)
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+    return render(request, 'auth/register.html', context)
--- carts/views.py
+++ carts/views.py
@@ -25,6 +25,10 @@ def cart_update(request):
     return redirect("cart:home")
 
 def checkout_home(request):
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
-    order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    order_obj = None
+    if cart_created or cart_obj.products.count() == 0:
+        return redirect("cart:home")
+    else:
+        order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
     return render(request, 'carts/checkout.html', {'object': order_obj})
