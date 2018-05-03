4c2f57520541c96c2a514bcd7ba7fa5bc2799b7a
daniel <bro.danielx@gmail.com>
2018-05-03 07:19:51 -0400

    refresh cart ajax part 3

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,8 +22,6 @@ from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.views.generic import TemplateView
 
-from carts.views import cart_home
-
 from accounts.views import (
     login_page,
     register_page,
@@ -33,6 +31,7 @@ from addresses.views import (
     checkout_address_create_view,
     checkout_address_reuse_view,
 )
+from carts.views import cart_detail_api_view
 from .views import (
     home_page,
     about_page,
@@ -49,6 +48,7 @@ urlpatterns = [
     url(r'^checkout/address/reuse/$', checkout_address_reuse_view, name='checkout_address_reuse'),
     url(r'^register/guest/$', guest_register_view, name='guest_register'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^api/cart/$', cart_detail_api_view, name='api-cart'),
     url(r'^register/$', register_page, name='register'),
     url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
     url(r'^products/', include('products.urls', namespace='products')),
