f0b9de0946ae8172ae2a27053cec4ecd07506aac
daniel <bro.danielx@gmail.com>
2018-05-13 08:50:17 -0400

    improving payment method form

--- billing/views.py
+++ billing/views.py
@@ -1,4 +1,6 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.utils.http import is_safe_url
 
 import stripe
 stripe.api_key = 'sk_test_xhUCOpSvefHALlsGQufxUfm3'
@@ -6,6 +8,15 @@ STRIPE_PUB_KEY = 'pk_test_HtxVLz47gz3gHg5Y6oD2e0p3'
 
 
 def payment_method_view(request):
-    if request.method == 'POST':
+    next_url = None
+    next_ = request.GET.get('next')
+    if is_safe_url(next_, request.get_host()):
+        next_url = next_
+
+    return render(request, 'billing/payment-method.html', {'publish_key': STRIPE_PUB_KEY, 'next_url': next_url})
+
+def payment_method_createview(request):
+    if request.method == 'POST' and request.is_ajax():
         print(request.POST)
-    return render(request, 'billing/payment-method.html', {'publish_key': STRIPE_PUB_KEY})
+        return JsonResponse({'message': 'Done'})
+    return HttpResponse('error', status_code=401)
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -31,7 +31,7 @@ from addresses.views import (
     checkout_address_create_view,
     checkout_address_reuse_view,
 )
-from billing.views import payment_method_view
+from billing.views import payment_method_view, payment_method_createview
 from carts.views import cart_detail_api_view
 from .views import (
     home_page,
@@ -51,6 +51,7 @@ urlpatterns = [
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^api/cart/$', cart_detail_api_view, name='api-cart'),
     url(r'^billing/payment-method/$', payment_method_view, name='billing-payment-method'),
+    url(r'^billing/payment-method/create/$', payment_method_createview, name='billing-payment-method-endpoint'),
     url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
     url(r'^products/', include('products.urls', namespace='products')),
